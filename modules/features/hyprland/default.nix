3dedfcc86cb951dc5502730048a59f08bad3e4e3
Simon Pitt <si_pitt@yahoo.com>
2026-08-09 23:40:30 +0100

    nixos: tidy hyprland config

--- modules/features/hyprland/default.nix
+++ modules/features/hyprland/default.nix
@@ -1,19 +1,6 @@
 {
   flake.homeModules.hyprland =
     { pkgs, ... }:
-    # let
-    #   wlogout' = pkgs.wlogout.overrideAttrs (
-    #     finalAttrs: prevAttrs: {
-    #       postPatch = prevAttrs.postPatch + ''
-    #         substituteInPlace layout \
-    #           --replace-fail "loginctl lock-session" "swaylock"
-    #
-    #         substituteInPlace layout \
-    #           --replace-fail  "loginctl terminate-user \$USER" "uwsm stop"
-    #       '';
-    #     }
-    #   );
-    # in
     {
 
       wayland.windowManager.hyprland = {
@@ -60,70 +47,10 @@
 
       };
 
-      # services.hyprpaper = {
-      #   enable = true;
-      #   settings = {
-      #     splash = true;
-      #     splash_offset = 1;
-      #     preload = [
-      #       "/home/simon/.nixos/_assets/nixos-wallpaper-catppuccin-mocha.png"
-      #     ];
-      #     wallpaper = [
-      #       {
-      #         monitor = "eDP-1";
-      #         path = "/home/simon/.nixos/_assets/nixos-wallpaper-catppuccin-mocha.png";
-      #         fit_mode = "cover";
-      #       }
-      #       {
-      #         monitor = "";
-      #         path = "/home/simon/.nixos/_assets/nixos-wallpaper-catppuccin-mocha.png";
-      #         fit_mode = "cover";
-      #       }
-      #     ];
-      #   };
-      # };
-
-      # services.hypridle = {
-      #   enable = true;
-      #   settings = {
-      #     general = {
-      #       ignore_dbus_inhibit = false;
-      #       ignore_systemd_inhibit = false;
-      #     };
-      #     listener = [
-      #       {
-      #         timeout = 300;
-      #         on-timeout = "swaylock";
-      #       }
-      #       {
-      #         timeout = 600;
-      #         on-timeout = "hyprctl dispatch dpms off";
-      #         on-resume = "hyprctl dispatch dpms on";
-      #       }
-      #     ];
-      #   };
-      # };
-
-      # programs.swaylock = {
-      #   enable = true;
-      #   settings = {
-      #     image = "${./../../../_assets/nix-wallpaper-binary-black.png}";
-      #     color = "1e1e2e";
-      #     font-size = 18;
-      #     indicator-idle-visible = false;
-      #     indicator-radius = 100;
-      #     line-color = "74c7ec";
-      #     show-failed-attempts = true;
-      #   };
-      # };
-
       home.packages = with pkgs; [
         hyprpaper
         hyprshot
         hyprpicker
-        # swaylock
-        # waybar
-        # wlogout'
         xdg-desktop-portal-gtk
       ];
 
@@ -131,7 +58,6 @@
         HYPRSHOT_DIR = "/home/simon/Pictures/screenshots";
         # Optional, hint Electron apps to use Wayland:
         NIXOS_OZONE_WL = "1";
-        WGPU_ADAPTER_NAME = "AMD Radeon RX 7800 XT (RADV NAVI32)";
         # VK_ICD_FILENAMES etc are set in the whisper module.
         GDK_BACKEND = "wayland";
         WLR_NO_HARDWARE_CURSORS = "1";
